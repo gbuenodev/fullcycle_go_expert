a945aa315f9e863d6df6fc9a963dad41f9b2b243
Gabriel Bueno <gabrielsbueno@hotmail.com>
2025-12-01 10:04:02 -0300

    desafio 05 - initial configs

---
+++ desafio05/cmd/auction/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/gbuenodev/fullcycle_go_expert/desafio05/configs/database/mongodb"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	ctx := context.Background()
+
+	if err := godotenv.Load("cmd/auction/.env"); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+		return
+	}
+
+	db, err := mongodb.NewMongoDBConnection(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	defer db.Client().Disconnect(ctx)
+}
