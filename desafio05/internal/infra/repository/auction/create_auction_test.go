67f7e578e5b1862007a2ce25e71f338f92e4b922
Gabriel Bueno <gabrielsbueno@hotmail.com>
2025-12-02 21:03:00 -0300

    test - finishes implementing tests

--- desafio05/internal/infra/repository/auction/create_auction_test.go
+++ desafio05/internal/infra/repository/auction/create_auction_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 	"time"
 
+	auctionentity "github.com/gbuenodev/fullcycle_go_expert/desafio05/internal/entity/auction_entity"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
 )
 
@@ -16,16 +18,51 @@ func setupTest(t *testing.T) *mtest.T {
 func TestCompleteAuction_Success(t *testing.T) {
 	mt := setupTest(t)
 
-	mt.Run("sets status to Completed after timeout", func(mt *mtest.T) {
+	mt.Run("tests complete auction", func(mt *mtest.T) {
 		os.Setenv("AUCTION_DURATION", "50ms")
 		defer os.Unsetenv("AUCTION_DURATION")
 
-		repo := &AuctionRepository{Collection: mt.Coll}
-		mt.AddMockResponses(mtest.CreateSuccessResponse())
+		auction, err := auctionentity.NewAuction(
+			"test-product",
+			"test-category",
+			"test-description",
+			auctionentity.New,
+		)
+		if err != nil {
+			t.Fatalf("NewAuction() error = %v", err)
+		}
 
-		go repo.completeAuction(context.Background(), "test-id")
+		mt.AddMockResponses(
+			mtest.CreateSuccessResponse(),
+			mtest.CreateSuccessResponse(),
+			mtest.CreateCursorResponse(1, "auctions.auctions", mtest.FirstBatch, bson.D{
+				{Key: "_id", Value: auction.ID},
+				{Key: "product_name", Value: auction.ProductName},
+				{Key: "category", Value: auction.Category},
+				{Key: "description", Value: auction.Description},
+				{Key: "condition", Value: auction.Condition},
+				{Key: "status", Value: auctionentity.Completed},
+				{Key: "timestamp", Value: auction.Timestamp.Unix()},
+			}),
+		)
+
+		repo := &AuctionRepository{Collection: mt.Coll}
+
+		err = repo.CreateAuction(context.Background(), auction)
+		if err != nil {
+			t.Fatalf("CreateAuction() error = %v", err)
+		}
 
 		time.Sleep(100 * time.Millisecond)
+
+		completedAuction, err := repo.FindAuctionById(context.Background(), auction.ID)
+		if err != nil {
+			t.Fatalf("FindAuctionById() error = %v", err)
+		}
+
+		if completedAuction.Status != auctionentity.Completed {
+			t.Errorf("Auction status = %v, want %v", completedAuction.Status, auctionentity.Completed)
+		}
 	})
 }
 
